11c6d47466b6917e5401604c10e85f234be39b56
bodia1132 <bohdan.baran.itis.2018@lpnu.ua>
2020-11-25 18:44:01 +0200

    Laba3

--- Laba 3/main/views.py
+++ Laba 3/main/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import os
-import datetime
+from datetime import datetime
 
 
 def main(request):
-    return render(request, 'main.html')
+    return render(request, 'main.html', {'parameter': "test"})
 
 
 def health(request):
-    response = {
-        'date': datetime.datetime.now(),
-        'current_page': request.build_absolute_uri(),
-        'server_info': os.name,
-        'client_info': request.META['HTTP_USER_AGENT']
-    }
+    now = datetime.now()
+    response = {'date': now.strftime("%d/%m/%Y %H:%M:%S"), 'current_page': request.build_absolute_uri(), 'server_info': os.uname(), 'client_info': request.headers['User-Agent']}
     return JsonResponse(response)
